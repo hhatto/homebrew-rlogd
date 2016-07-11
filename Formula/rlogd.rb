150261babc8eb166615ffc6b5a7efac93ead6c77
hhatto <hhatto.jp@gmail.com>
2016-07-12 01:03:03 +0900

    initical

--- Formula/rlogd.rb
+++ Formula/rlogd.rb
@@ -5,6 +5,8 @@ class Rlogd < Formula
   url 'https://github.com/pandax381/rlogd/archive/v0.4.6.tar.gz'
   sha256 'ca7dd203065d19b2b64d1bd688a10ac188afd9c94363d56fa0441d26c6609b84'
 
+  head 'https://github.com/pandax381/rlogd.git'
+
   depends_on :autoconf
   depends_on :automake
   depends_on :libtool
